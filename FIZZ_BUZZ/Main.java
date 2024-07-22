09894258755d55d21b24c85e5a816f5f4c825a24
Juan Vázquez <vbxoan@gmail.com>
2024-07-22 17:02:00 +0200

    FIZZ BUZZ #0

--- FIZZ_BUZZ/Main.java
+++ FIZZ_BUZZ/Main.java
@@ -12,7 +12,7 @@ package FIZZ_BUZZ;
 
 public class Main {
     public static void main(String[] args) {
-        for (int i = 1; i < 101; i++) {
+        for (int i = 1; i == 100; i++) {
             if (i % 3 == 0) {
                 System.out.println("fizz");
             } else if (i % 5 == 0) {
